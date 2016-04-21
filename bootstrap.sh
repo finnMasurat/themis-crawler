a53c17b3951c92f8f68a477b0031f6a352106e85
eneskaya <eneskaya@me.com>
2016-04-21 17:08:58 +0200

    Added language locale to bashrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+
+cd ~
+echo "export LC_ALL=\"en_US.UTF-8\"" >> .bashrc
+echo "export LC_CTYPE=\"en_US.UTF-8\"" >> .bashrc
+
+dpkg-reconfigure locales
+
+
+#######
+
+cd ~
+
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 
@@ -9,3 +21,4 @@ apt-get -y install python3-lxml python-lxml
 apt-get -y install python-dev python-pip libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
 
 pip install Scrapy
+pip install porc
