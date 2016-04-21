1e09b5930ceabc94343a9f6b4ca0755addbad9dc
Finn Masurat <finn.masurat@hotmail.de>
2016-04-21 19:05:45 +0200

    Crawler with correct attributes

--- crawler/themisbot/themisbot/spiders/nytimes.py
+++ crawler/themisbot/themisbot/spiders/nytimes.py
@@ -1,18 +1,27 @@
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
 from themisbot.items import NyTimesItem
+from scrapy.linkextractors import LinkExtractor
 
-class NyTimesSpider(scrapy.Spider):
-    name = "nytimesworld"
-    allowed_domains = "nytimes.com"
+class NyTimesSpider(CrawlSpider):
+    name = 'nytimes'
+    allowed_domains = ['nytimes.com']
 
-    start_urls = [
-        "http://www.nytimes.com/"
-    ]
+    start_urls = ['http://www.nytimes.com/']
 
-    def parse(self, response):
+    rules = (
+        Rule(
+            LinkExtractor(
+                allow=r'http://www\.nytimes\.com/[0-9][0-9][0-9][0-9]/[0-9][0-9]/[0-9][0-9]/[a-zA-Z]+/.*'),
+                callback='parse_item',
+                follow=True
+            ),
+    )
+
+    def parse_item(self, response):
         item = NyTimesItem()
         item['title'] = response.xpath('//title/text()').extract()[0]
         item['content'] = response.css('div.story-body').xpath('//p/text()').extract()
-        item['author'] = "get_author"
-        item['date'] = "get_date"
-        print item
+        item['author'] = response.css("#story-meta-footer span.byline-author::attr('data-byline-name')").extract()
+        item['date'] = response.css("#story-meta-footer time.dateline::attr('datetime')").extract()
+        print response.url
