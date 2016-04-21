3d82b297370aa9ae16f35471000a3c364c6abb89
eneskaya <eneskaya@me.com>
2016-04-21 17:15:38 +0200

    Update per apt-get in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+apt-get update
 
 cd ~
 echo "export LC_ALL=\"en_US.UTF-8\"" >> .bashrc
 echo "export LC_CTYPE=\"en_US.UTF-8\"" >> .bashrc
 
 dpkg-reconfigure locales
-
 
 #######
 
