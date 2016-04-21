223bb133cc971e790f3991611950a7b4bf7a3257
eneskaya <eneskaya@me.com>
2016-04-21 16:08:16 +0200

    Bootstrap.sh installs scrapy for you

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+
+wget https://bootstrap.pypa.io/ez_setup.py -O - | python
+
+apt-get -y install python3-lxml python-lxml
+apt-get -y install python-dev python-pip libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
+
+pip install Scrapy
